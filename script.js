ca1caf3f558f4735d2787c610277f92d3bee917d
Dima <dima2003strik@gmail.com>
2024-12-06 15:58:33 +0200

    fisrt commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert("Hello!");
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answers = [];
+
+// answers[0] = prompt('Whats your first name', '');
+// answers[1] = prompt('Whats your second name', '');
+// answers[2] = prompt('How old are you', '');
+
+// console.log(answers);
+
+const user = "Dima";
+alert(`Привет, ${user}`);
